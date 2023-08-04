30bed13a4109deb82ade29ebb401564c0baf3761
nakayu08 <nakabo.yu08@gmail.com>
2023-08-04 17:08:52 +0900

    ログイン後のタスク表示画面作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_04_071634) do
+ActiveRecord::Schema.define(version: 2023_08_04_075926) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
