4c75123177fae7b84b30f4836876d993e7409ac8
nakayu08 <nakabo.yu08@gmail.com>
2023-08-07 13:52:48 +0900

    中間テーブルのマイグレーション

--- db/migrate/20230807044629_create_task_users.rb
+++ db/migrate/20230807044629_create_task_users.rb
@@ -1,7 +1,8 @@
 class CreateTaskUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :task_users do |t|
-
+      t.references :task, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
