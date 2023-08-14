bcb35722bf148097c60874e4f454a5a055c40234
nakayu08 <nakabo.yu08@gmail.com>
2023-08-14 18:27:19 +0900

    自分に関わりのあるタスクだけ表示する修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,9 @@
 class TasksController < ApplicationController
   def index
     @users = User.all
-    @tasks  = Task.all
+    @user = User.find(current_user.id)
+    #binding.pry
+    @tasks  = @user.tasks
   end
 
   def new
