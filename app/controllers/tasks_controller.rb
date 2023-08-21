122e29d9e5a612665692eb5fa07c96519cbeb1b6
nakayu08 <nakabo.yu08@gmail.com>
2023-08-21 17:22:32 +0900

    updateのアクションの設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,6 +31,15 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def update
+    task = Task.find(params[:id])
+    if task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def task_params
