0739c17b294a9021e24556b51ffb47bd9049278f
nakayu08 <nakabo.yu08@gmail.com>
2023-08-27 18:00:37 +0900

    commentsコントローラーを編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to task_path(params[:task_id])
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, task_id: params[:task_id])
+  end
 end
