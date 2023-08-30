775f79978356fe62a6ff0d5600b26729a110e68a
nakayu08 <nakabo.yu08@gmail.com>
2023-08-30 09:37:05 +0900

    コントローラーの追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,17 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @task = Task.find(params[:task_id])
+    comment = @task.comments.find(params[:id])
+    if current_user.id == comment.user.id
+      comment.destroy
+      redirect_to edit_task_path(params[:task_id])
+    else
+      redirect_to tasks_path
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, task_id: params[:task_id])
