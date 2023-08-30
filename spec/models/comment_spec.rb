2eb480ab0bf28fcf09d2791f64f5a017022b8720
nakayu08 <nakabo.yu08@gmail.com>
2023-08-30 10:01:51 +0900

    コメント機能の単体テストの作成

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント投稿' do
+    context 'コメントが投稿できる場合' do
+      it 'textが存在していれば保存できる' do
+        expect(@comment).to be_valid
+      end
+    end
+    context 'メッセージが投稿できない場合' do
+      it 'textが空では保存できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text を入力してください")
+
+      end
+      it 'taskが紐付いていないと保存できない' do
+        @comment.task = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Task must exist")
+
+      end
+      it 'userが紐付いていないと保存できない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("User must exist")
+
+      end
+    end
+  end
 end
