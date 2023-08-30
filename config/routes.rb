1570be529c5bacb022fe41b675d849b3314267ee
nakayu08 <nakabo.yu08@gmail.com>
2023-08-30 09:10:35 +0900

    コメントの削除機能のルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   resources :tasks do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
   end
 end
