f5589b50f7c7706598f3e9bff45f0d3b52b006bc
nakayu08 <nakabo.yu08@gmail.com>
2023-09-02 21:32:44 +0900

    テストコードへの追記

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,20 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   before do
-    @task = FactoryBot.build(:task)
+    user = FactoryBot.create(:user)
+    @task = FactoryBot.build(:task,user_ids: user.id)
   end
 
   describe 'タスク登録' do
     context 'タスク登録できる場合' do
       it 'title、start_time、end_time、category、condition、explanationが存在していれば保存できる' do
-        expect(@comment).to be_valid
+        expect(@task).to be_valid
       end
     end
     context 'タスク登録できない場合' do
       it 'titleが空では保存できない' do
-    
+        @task.title = ''
+        @task.valid?
+        expect(@task.errors.full_messages).to include "タスク名を入力してください"
       end
       it 'start_timeが空では保存できない' do
+        @task.title = ''
+        @task.valid?
+        expect(@task.errors.full_messages).to include "タスク名を入力してください"
+
 
       end
       it 'end_timeが空では保存できない' do
