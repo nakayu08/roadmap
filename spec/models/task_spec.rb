4a0d7480fefab9f63a6ed688bee740e6972306b5
nakayu08 <nakabo.yu08@gmail.com>
2023-09-05 16:33:12 +0900

    タスク登録機能の単体テストの修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   before do
-    user=FactoryBot.create(:user)
-    @task = FactoryBot.build(:task,user_ids: [user.id])
+    @user=FactoryBot.create(:user)
+    @task = FactoryBot.build(:task,user_ids: [@user.id])
   end
 
   describe 'タスク登録' do
@@ -21,38 +21,56 @@ RSpec.describe Task, type: :model do
       it 'start_timeが空では保存できない' do
         @task.start_time = ''
         @task.valid?
-        expect(@task.errors.full_messages).to include "は開始時刻より遅い時間を選択してください"
+        expect(@task.errors.full_messages).to include "開始時間を入力してください"
       end
       it 'end_timeが空では保存できない' do
+        @task.end_time = ''
+        @task.valid?
+        expect(@task.errors.full_messages).to include "終了時間を入力してください"
       
-
       end
       it 'start_timeよりend_timeが早い場合は保存できない' do
-
+        @task.end_time = @task.start_time - 10
+        @task.valid?
+        expect(@task.errors.full_messages).to include "終了時間は開始時刻より遅い時間を選択してください"
       end
 
       it 'categoryが空では保存できない' do
+        @task.category_id = ''
+        @task.valid?
+        expect(@task.errors.full_messages).to include "タスクの種類を選択してください"
 
       end
       it 'categoryが1では保存できない' do
+        @task.category_id = 1
+        @task.valid?
+        expect(@task.errors.full_messages).to include "タスクの種類を選択してください"
 
       end
       it 'conditionが空では保存できない' do
+        @task.condition_id = ''
+        @task.valid?
+        expect(@task.errors.full_messages).to include "タスクの状態を選択してください"
 
       end
       it 'conditionが1では保存できない' do
+        @task.condition_id = ''
+        @task.valid?
+        expect(@task.errors.full_messages).to include "タスクの状態を選択してください"
 
       end
 
       it 'explanationが空では保存できない' do
-
+        @task.explanation = ''
+        @task.valid?
+        expect(@task.errors.full_messages).to include "タスクの説明を入力してください"
       end
 
       it 'userが紐付いていないと保存できない' do
-
+        @task.user_ids = nil
+        @task.valid?
+        expect(@task.errors.full_messages).to include "タスクの参加者を入力してください"
       end
-
-
     end
   end
 end
